cb5c848a490f65d924999a14db25c970b526c0ef
João Pedro Gonçalves <72882679+jpgoncalves13@users.noreply.github.com>
2023-10-26 13:23:59 +0100

    Delete json.py

--- json.py
+++
@@ -1,9 +0,0 @@
-import simplejson as json
-
-# Ler um arquivo JSON
-with open('json/topologia_overlay.json', 'r') as f:
-    data = json.load(f)
-
-json.dumps(data, indent=4, sort_keys=True)
-
-print(data['nodes'])
