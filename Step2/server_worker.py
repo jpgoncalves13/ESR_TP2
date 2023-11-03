7463d05cf4349e461e9bec3557ea1568ea7d7dad
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-11-03 12:36:42 +0000

    Remove match

--- Step2/server_worker.py
+++ Step2/server_worker.py
@@ -17,20 +17,19 @@ class ServerWorker:
         if debug:
             print(f"DEBUG: Processing response to packet of type {packet.type}")
 
-        match packet.type:
-            case PacketType.SETUP:
+        if packet.type == PacketType.SETUP:
+            if debug:
+                print("DEBUG: Received a request to join the topology")
+            if bootstrapper is not None:
+                neighbours = bootstrapper.handle_join_request(packet.origin)
+                packet = Packet(packet.origin, neighbours, PacketType.RSETUP, [], 1, 1, 1)
+                socket_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                socket_s.sendto(packet.serialize(), (packet.origin, 5000))
+            else:
                 if debug:
-                    print("DEBUG: Received a request to join the topology")
-                if bootstrapper is not None:
-                    neighbours = bootstrapper.handle_join_request(packet.origin)
-                    packet = Packet(packet.origin, neighbours, PacketType.RSETUP, [], 1, 1, 1)
-                    socket_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    socket_s.sendto(packet.serialize(), (packet.origin, 5000))
-                else:
-                    if debug:
-                        print("ERROR: Bootstrapper is not running")
-            case PacketType.RSETUP:
-                if debug:
-                    print('DEBUG: Response received')
-                print(Packet.deserialize(request).destinations)
+                    print("ERROR: Bootstrapper is not running")
+        elif packet.type == PacketType.RSETUP:
+            if debug:
+                print('DEBUG: Response received')
+            print(Packet.deserialize(request).destinations)
 
