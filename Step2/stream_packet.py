bc4d74fc995262d3b78aa343a6d489489864e225
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-11-03 20:14:58 +0000

    Packet correction

--- Step2/stream_packet.py
+++ Step2/stream_packet.py
@@ -26,11 +26,11 @@ class Packet:
 
         self.origin = origin
         self.type = message_type
-        self.neighbors = neighbours
-        self.last_hops = last_hops
         self.delay = delay
         self.loss = loss
         self.number_of_hops = number_of_hops
+        self.last_hops = last_hops
+        self.neighbors = neighbours
         self.payload = payload
 
     def serialize(self):
@@ -40,18 +40,7 @@ class Packet:
         byte_array += self.type.value.to_bytes(1, byteorder='big')
 
         # origin
-        byte_array += len(self.origin).to_bytes(2, byteorder='big')
         byte_array += self.origin.encode('ascii')
-
-        # neighbors
-        byte_array += len(self.neighbors).to_bytes(4, byteorder='big')
-        for neighbor in self.neighbors:
-            byte_array += neighbor.encode('ascii')
-
-        # last hops
-        byte_array += len(self.last_hops).to_bytes(4, byteorder='big')
-        for hop in self.last_hops:
-            byte_array += hop.encode('ascii')
 
         # delay
         byte_array += self.delay.to_bytes(4, byteorder='big')
@@ -61,6 +50,16 @@ class Packet:
 
         # number of hops
         byte_array += self.number_of_hops.to_bytes(4, byteorder='big')
+
+        # last hops
+        byte_array += len(self.last_hops).to_bytes(4, byteorder='big')
+        for hop in self.last_hops:
+            byte_array += hop.encode('ascii')
+
+        # neighbors
+        byte_array += len(self.neighbors).to_bytes(4, byteorder='big')
+        for neighbor in self.neighbors:
+            byte_array += neighbor.encode('ascii')
 
         # payload
         byte_array += len(self.payload).to_bytes(4, byteorder='big')
@@ -82,16 +81,17 @@ class Packet:
         origin = byte_array[offset:offset + length_origin].decode('ascii')
         offset += length_origin
 
-        # Deserialize neighbors (array of strings)
-        neighbours_count = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
+        # Deserialize delay (4 bytes)
+        delay = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
         offset += 4
-        neighbours = []
-        for _ in range(neighbours_count):
-            str_length = int.from_bytes(byte_array[offset:offset + 2], byteorder='big')
-            offset += 2
-            neighbour = byte_array[offset:offset + str_length].decode('ascii')
-            offset += str_length
-            neighbours.append(neighbour)
+
+        # Deserialize loss (1 byte)
+        loss = int.from_bytes(byte_array[offset:offset + 1], byteorder='big')
+        offset += 1
+
+        # Deserialize number of hops (4 bytes)
+        number_of_hops = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
+        offset += 4
 
         # Deserialize last hops (array of strings)
         last_hops_count = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
@@ -104,17 +104,16 @@ class Packet:
             offset += str_length
             last_hops.append(hop)
 
-        # Deserialize delay (4 bytes)
-        delay = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
+        # Deserialize neighbors (array of strings)
+        neighbours_count = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
         offset += 4
-
-        # Deserialize loss (1 byte)
-        loss = int.from_bytes(byte_array[offset:offset + 1], byteorder='big')
-        offset += 1
-
-        # Deserialize number of hops (4 bytes)
-        number_of_hops = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
-        offset += 4
+        neighbours = []
+        for _ in range(neighbours_count):
+            str_length = int.from_bytes(byte_array[offset:offset + 2], byteorder='big')
+            offset += 2
+            neighbour = byte_array[offset:offset + str_length].decode('ascii')
+            offset += str_length
+            neighbours.append(neighbour)
 
         # Payload
         payload_bytes_count = int.from_bytes(byte_array[offset:offset + 4], byteorder='big')
