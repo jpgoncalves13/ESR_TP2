14aef5fa59e6ca19fe38ac9d171a4d68e3a13c17
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-11-09 19:23:58 +0000

    Remove rsetup type function

--- onode/server/server_worker.py
+++ onode/server/server_worker.py
@@ -12,32 +12,24 @@ class ServerWorker:
     def run(self, request):
         Thread(target=lambda: self.handle_request(request)).start()
 
-    def handle_rsetup(self, request):
-        if self.ep.debug:
-            print('DEBUG: Response received')
-        print(Packet.deserialize(request).destinations)
-
-    def handle_join(self, request):
-        if self.ep.debug:
-            print('DEBUG: Tree join received')
-
-    def handle_measure(self, packet):
+    def handle_join(self, packet):
         pass
 
-    def handle_rmeasure(self, packet):
-        if self.ep.debug:
-            print('DEBUG: Response to measure received')
+    def handle_measure(self, packet, ip):
+        packet.type = PacketType.RMEASURE
+        socket_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_s.sendto(packet.serialize(), (ip, self.ep.port))
 
+    def handle_rmeasure(self, packet):
         self.ep.table.update_delay(packet.origin, time.time() - packet.delay)
         self.ep.table.update_packets_received(packet.origin)
 
     def handle_setup(self, request):
-        if self.ep.debug:
-            print("DEBUG: Received a request to join the topology")
         request_neighbours = self.ep.bootstrapper.handle_join_request(request[1][0])
         packet = Packet('', PacketType.RSETUP, 1, 1, 1, neighbours=request_neighbours)
         socket_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_s.sendto(packet.serialize(), request[1])
+        socket_s.close()
 
     def handle_stream(self, request):
         pass
@@ -49,12 +41,10 @@ class ServerWorker:
             print(f"DEBUG: Processing response to packet of type {packet.type}")
 
         if self.ep.bootstrapper is None:
-            if packet.type == PacketType.RSETUP:
-                self.handle_rsetup(request)
-            elif packet.type == PacketType.JOIN:
-                self.handle_join(request)
+            if packet.type == PacketType.JOIN:
+                self.handle_join(packet)
             elif packet.type == PacketType.MEASURE:
-                self.handle_measure(packet)
+                self.handle_measure(packet, request[1][0])
             elif packet.type == PacketType.RMEASURE:
                 self.handle_rmeasure(packet)
             elif packet.type == PacketType.STREAM:
