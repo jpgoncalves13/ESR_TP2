089b1f41cc7ea2819be8bc985ca0d06f4f30fdf8
João Pedro Gonçalves <a95019@alunos.uminho.pt>
2023-11-11 12:00:43 +0000

    Atualização no cliente e servidor

--- Step3/Client.py
+++ Step3/Client.py
@@ -278,7 +278,7 @@ class Client:
 		
 		try:
 			# Bind the socket to the address using the RTP port given by the client user
-			self.rtpSocket.bind(('127.0.0.1', self.rtpPort))
+			self.rtpSocket.bind((self.serverAddr, self.rtpPort))
 		except:
 			messagebox.showwarning('Unable to Bind', 'Unable to bind PORT=%d' %self.rtpPort)
 
--- Step3/Server.py
+++ Step3/Server.py
@@ -6,11 +6,12 @@ class Server:
 	
 	def main(self):
 		try:
+			SERVER_IP = sys.argv[1]
 			SERVER_PORT = int(sys.argv[1])
 		except:
 			print("[Usage: Server.py Server_port]\n")
 		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		rtspSocket.bind(('127.0.0.1', SERVER_PORT))
+		rtspSocket.bind((SERVER_IP, SERVER_PORT))
 		rtspSocket.listen(5)        
 		# Receive client info (address,port) through RTSP/TCP session
 		while True:
