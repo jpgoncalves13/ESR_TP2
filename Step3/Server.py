60c18ff97a1488308005386d0b46b1d61a7d29dc
jpgoncalves13 <jpagoncalves7@gmail.com>
2023-11-11 13:49:52 +0000

    Pequena alteracao

--- Step3/Server.py
+++ Step3/Server.py
@@ -7,7 +7,7 @@ class Server:
 	def main(self):
 		try:
 			#SERVER_IP = sys.argv[1]
-			SERVER_PORT = int(sys.argv[2])
+			SERVER_PORT = int(sys.argv[1])
 		except:
 			print("[Usage: Server.py Server_port]\n")
 		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
