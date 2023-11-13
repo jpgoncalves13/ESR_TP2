dd85829cba3881e99b70ef0603c04039ab5d604a
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-11-13 18:56:08 +0000

    Correction

--- onode/server/server_worker.py
+++ onode/server/server_worker.py
@@ -63,12 +63,11 @@ class ServerWorker:
             next_hop = self.ep.table.update_tree_entry(packet.leaf, packet.last_hop)
             neighbour = packet.last_hop
             packet.last_hop = next_hop
-            try:
-                socket_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                socket_s.sendto(packet.serialize(), (neighbour, self.ep.port))
-                socket_s.close()
-            except Exception as e:
-                print(f"Erro ao enviar pacote: {e}")
+            udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            if self.ep.debug:
+                print(f"Sending packet to (f{neighbour},f{self.ep.port})")
+            udp_socket.sendto(packet.serialize(), (neighbour, self.ep.port))
+            udp_socket.close()
 
     def handle_request(self, request):
         packet = Packet.deserialize(request[0])
