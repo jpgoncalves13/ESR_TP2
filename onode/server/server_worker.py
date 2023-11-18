a4ce297d0d4ec159c8c71f0818e37f231cfa40af
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-11-18 23:45:01 +0000

    Debug print for measure

--- onode/server/server_worker.py
+++ onode/server/server_worker.py
@@ -76,9 +76,9 @@ class ServerWorker:
     def handle_measure(self, address):
         best_entries_list = self.ep.get_best_entries()
         if self.ep.rendezvous:
-            best_entries_list += ("RP", "0.0.0.0", 0, 0)
+            best_entries_list.append(("RP", "0.0.0.0", 0, 0))
         if len(self.ep.get_neighbours()) == 1 and not self.ep.rendezvous:
-            best_entries_list += (self.ep.node_id, "0.0.0.0", 0, 0)
+            best_entries_list.append((self.ep.node_id, "0.0.0.0", 0, 0))
 
         if self.ep.debug:
             print("DEBUG: " + str(best_entries_list))
