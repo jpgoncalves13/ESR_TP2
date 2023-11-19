32fcbc444b351bc722ed82c87306e4742551b3a8
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-11-19 16:54:30 +0000

    Update test script

--- onode/test_client.py
+++ onode/test_client.py
@@ -1,7 +1,7 @@
 from server.stream_packet import Packet, PacketType
 import socket
 
-packet = Packet(PacketType.STREAMREQ, '0.0.0.0', '', 0, '0.0.0.0')
+packet = Packet(PacketType.STREAMREQ, '0.0.0.0', 0, 0, '0.0.0.0')
 upd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ip = input()
 upd.sendto(packet.serialize(), (ip, 5000))
