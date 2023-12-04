2d45fdb2bacd4b93beebc50576a8207a82b266a1
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-12-04 22:11:42 +0000

    Correct error

--- onode/server/probe_thread.py
+++ onode/server/probe_thread.py
@@ -86,6 +86,7 @@ class ProbeThread(threading.Thread):
             for neighbour in neighbours:
                 loss_measured, delay_measured, last_packet = self.measure(neighbour)
                 if last_packet is not None and last_packet.type == PacketType.RMEASURE:
+                    print(f"PACKET TO UPDATE: f{last_packet}")
                     self.handle_neighbours(neighbour, last_packet, delay_measured, loss_measured)
                 else:
                     self.handle_neighbours_total_loss(neighbour)
