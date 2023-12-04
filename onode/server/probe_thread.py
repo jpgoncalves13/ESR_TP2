d52e4378588014582a3ffc1f401cb6c43defc452
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-12-04 22:29:47 +0000

    Correct error

--- onode/server/probe_thread.py
+++ onode/server/probe_thread.py
@@ -20,6 +20,7 @@ class ProbeThread(threading.Thread):
         list_metrics, rp_entry = last_packet.payload
         leafs_received = [tup[0] for tup in list_metrics]
         if len(leafs_received) > 0:
+            print(leafs_received)
             self.ep.remove_clients_neighbour_from_forwarding_table(leafs_received, neighbour)
         for leaf, next_hop, delay, loss in list_metrics:
             self.ep.update_metrics(neighbour if leaf == "0.0.0.0" else leaf, neighbour, next_hop,
@@ -86,7 +87,6 @@ class ProbeThread(threading.Thread):
             for neighbour in neighbours:
                 loss_measured, delay_measured, last_packet = self.measure(neighbour)
                 if last_packet is not None and last_packet.type == PacketType.RMEASURE:
-                    print(f"PACKET TO UPDATE: f{last_packet}")
                     self.handle_neighbours(neighbour, last_packet, delay_measured, loss_measured)
                 else:
                     self.handle_neighbours_total_loss(neighbour)
