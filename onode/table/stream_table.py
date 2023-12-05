b45d8b66d3de30d48ba5f73d5d7e0933a10150a9
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-12-05 20:09:46 +0000

    Update stream table lock

--- onode/table/stream_table.py
+++ onode/table/stream_table.py
@@ -12,16 +12,18 @@ class StreamTable:
     def __init__(self):
         self.table = {}  # Table of servers and neighbours for each stream, stream_id : (servers, neighbours)
         self.servers_entries = {}
-        self.lock = threading.Lock()
+        self.table_lock = threading.Lock()
+        self.entries_lock = threading.Lock()
 
     """
     Adds a neighbour to a stream
     If the stream does not exist, add a new entry in the map with a new neighbour
     """
     def add_neighbour_to_stream(self, stream_id, neighbour):
-        with self.lock:
+        with self.table_lock:
             if stream_id not in self.table:
                 self.table[stream_id] = (set(), set())
+
             self.table[stream_id][1].add(neighbour)
     
     """
@@ -29,7 +31,7 @@ class StreamTable:
     Returns if it was the last neighbour from that stream 
     """         
     def remove_neighbour_from_stream(self, stream_id, neighbour_ip):
-        with self.lock:
+        with self.table_lock:
             self.table[stream_id][1].discard(neighbour_ip)
             return len(self.table[stream_id][1]) == 0
 
@@ -38,25 +40,27 @@ class StreamTable:
     If the stream does not exist, add a new entry in the map with a new neighbour
     """
     def add_server_to_stream(self, stream_id, server_ip):
-        with self.lock:
+        with self.table_lock:
             if stream_id not in self.table:
                 self.table[stream_id] = (set(), set())
             self.table[stream_id][0].add(server_ip)
-            
-            # Add the server to the server measure list 
+
+        with self.entries_lock:
+            # Add the server to the server measure list
             if server_ip not in self.servers_entries:
                 self.servers_entries[server_ip] = TableEntry(0, 0)
-                
+
     """
     Remove a server from a stream
     """
     def remove_server_from_stream(self, server_ip):
-        with self.lock:
+        with self.table_lock:
             for stream_id in self.servers_entries:
                 servers, _ = self.servers_entries[stream_id]
                 servers.discard(server_ip)
                 break
 
+        with self.entries_lock:
             if server_ip in self.servers_entries:
                 del self.servers_entries[server_ip]
             
@@ -64,21 +68,21 @@ class StreamTable:
     Check if a stream is already in the stream list
     """
     def check_if_stream_exists(self, stream_id):
-        with self.lock:
+        with self.table_lock:
             return stream_id in self.table
 
     """
     Get all the servers currently streaming
     """
     def get_servers(self):
-        with self.lock:
+        with self.entries_lock:
             return list(self.servers_entries.keys())
         
     """
     Get the neighbours for a given stream
     """
     def get_stream_neighbours(self, stream_id):
-        with self.lock:
+        with self.table_lock:
             if stream_id in self.table:
                 return self.table[stream_id][1]
             return []
@@ -87,26 +91,27 @@ class StreamTable:
     Update the metrics associated with a given server (server, delay, loss)
     """
     def update_metrics_server(self, server, delay, loss):
-        with self.lock:
+        with self.servers_entries:
             self.servers_entries[server] = TableEntry(delay, loss)
 
     """
     Return a true if it's the server with the best metric for a specific stream_id
     """
     def its_best_server(self, stream_id, server_ip):
-        with self.lock:
+        with self.table_lock:
             servers, clients = self.table[stream_id]
 
-            score = sys.maxsize
-            best_server = None
-            for server in servers:
+        score = sys.maxsize
+        best_server = None
+        for server in servers:
+            with self.entries_lock:
                 entry = self.servers_entries[server]
-                if entry.get_metric() < score:
-                    score = entry.get_metric()
-                    best_server = server
+            if entry.get_metric() < score:
+                score = entry.get_metric()
+                best_server = server
 
-            return best_server == server_ip
+        return best_server == server_ip
 
     def get_stream_table(self):
-        with self.lock:
+        with self.table_lock:
             return copy.deepcopy(self.table)
