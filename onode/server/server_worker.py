1113b689e9a82df4428f7ba42f5544f1d7ce6df6
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-12-05 15:09:39 +0000

    Measure and stream updated

--- onode/server/server_worker.py
+++ onode/server/server_worker.py
@@ -42,16 +42,8 @@ class ServerWorker:
         packet = Packet(PacketType.RSETUP, '0.0.0.0', 0, '0.0.0.0',
                         request_neighbours if request_neighbours is not None else [])
         ServerWorker.send_packet(packet, address)
-
-    def handle_hello(self, address):
-        """Neighbour listening"""
-        if address[0] in self.ep.get_neighbours():
-            self.ep.set_state_of_neighbour(address[0], True)
-            packet = Packet(PacketType.ACK, '0.0.0.0', 0, '0.0.0.0')
-            ServerWorker.send_packet(packet, address)
-
+    """
     def flood_packet(self, sender_ip, packet_serialized):
-        """Send a packet to all listening neighbours, except the one that sent the packet"""
         neighbours = self.ep.get_listening_neighbours()
         if sender_ip in neighbours:
             neighbours.remove(sender_ip)
@@ -59,6 +51,7 @@ class ServerWorker:
         for neighbour in neighbours:
             udp_socket.sendto(packet_serialized, (neighbour, self.ep.port))
         udp_socket.close()
+    """
 
     def handle_stream(self, packet, ip):
         if self.ep.rendezvous:
@@ -66,54 +59,18 @@ class ServerWorker:
         if self.ep.rendezvous and not self.ep.its_best_server(packet.stream_id, ip):
             return
 
-        stream_clients, data = packet.payload
-        
-        if self.ep.rendezvous:
-            stream_clients = self.ep.get_stream_clients(packet.stream_id)
-        # stream_clients = self.ep.get_stream_clients(packet.stream_id)
-        # neighbours_to_send = []
-        neighbours_to_send = {}
-        clients_to_send = []
-
-        for client in stream_clients:
-            entry = self.ep.get_best_entry(client)
-            neighbour = self.ep.get_neighbour_to_client(client)
-            if neighbour is not None:
-                if client == neighbour:
-                    if entry.loss == 100:
-                        # Send leave  because client is dead
-                        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                        leave_packet = Packet(PacketType.LEAVE, client, 0, '0.0.0.0')
-                        self.ep.remove_client_from_forwarding_table(client)
-                        ServerWorker.send_packet_with_confirmation(udp_socket, leave_packet.serialize(),
-                                                                   (self.ep.get_neighbour_to_rp(), self.ep.port))
-                    elif client not in clients_to_send:
-                        clients_to_send.append(client)
-
-                else:
-                    if entry.loss == 100:
-                        neighbour = entry.next_hop
-                    if neighbour not in neighbours_to_send:
-                        # neighbours_to_send.append(neighbour)
-                        neighbours_to_send[neighbour] = [client]
-                    else:
-                        neighbours_to_send[neighbour].append(client)
+        neighbours = self.ep.get_stream_neighbours(packet.stream_id)
 
         if self.ep.debug:
-            print("DEBUG: Packet sent to: " + str(neighbours_to_send))
-        if self.ep.debug:
-            print("DEBUG: Packet sent to: " + str(clients_to_send))
+            print(f"DEBUG: Stream packet sent to: {neighbours}")
+
+        if self.ep.client_on:
+            # Update the client video
+            pass
 
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        for neighbour in neighbours_to_send:
-            packet.payload = (neighbours_to_send[neighbour], data)
+        for neighbour in neighbours:
             udp_socket.sendto(packet.serialize(), (neighbour, self.ep.port))
-        udp_socket.close()
-
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        for client in clients_to_send:
-            packet.payload = ([], data)
-            udp_socket.sendto(packet.serialize(), (client, 5001))
         udp_socket.close()
 
     def handle_join(self, packet, ip):
@@ -162,24 +119,19 @@ class ServerWorker:
 
     def handle_measure(self, address):
         """Handle the packets requesting the metrics"""
-        if self.ep.get_num_neighbours() == 1 and not self.ep.rendezvous:
-            packet = Packet(PacketType.RMEASURE, '0.0.0.0', 0, '0.0.0.0',
-                            ([('0.0.0.0', '0.0.0.0', 0, 0)] if self.ep.get_client_state() else [], None))
-            ServerWorker.send_packet(packet, address)
-            return
 
         if self.ep.rendezvous:
-            best_entries_list = []
+            neighbours = []
             rp_entry = ('0.0.0.0', '0.0.0.0', 0, 0)
         else:
-            best_entries_list = self.ep.get_best_entries()
-            best_entries_list = [tup for tup in best_entries_list if tup[1] != address[0]]
             rp_entry = self.ep.get_best_entry_rp()
+            # UPDATE THIS
+            neighbours = []
             if rp_entry is not None and rp_entry[1] == address[0]:
                 rp_entry = None
 
         packet = Packet(PacketType.RMEASURE, '0.0.0.0', 0, '0.0.0.0',
-                        (best_entries_list, rp_entry))
+                        (rp_entry, neighbours))
         ServerWorker.send_packet(packet, address)
 
     def handle_request(self, response, address):
@@ -193,10 +145,7 @@ class ServerWorker:
                 print(f"DEBUG: Packet: {packet.payload[0]}")
 
         # Normal node
-        if packet.type == PacketType.HELLO:
-            self.handle_hello(address)
-
-        elif packet.type == PacketType.JOIN:
+        if packet.type == PacketType.JOIN:
             ServerWorker.send_packet(Packet(PacketType.ACK, '0.0.0.0', 0, '0.0.0.0'), address)
             self.handle_join(packet, address[0])
 
@@ -217,6 +166,4 @@ class ServerWorker:
         if self.ep.debug:
             print((self.ep.tag if self.ep.tag is not None else "") + " CLIENTS_TABLE" + str(self.ep.get_table()) + "\n")
             print("RP_TABLE" + str(self.ep.get_table_rp()) + "\n")
-            print("TREE" + str(self.ep.get_tree()) + "\n")
-            if self.ep.rendezvous:
-                print("STREAM_TABLE" + str(self.ep.get_stream_table()) + "\n")
+            print("STREAM_TABLE" + str(self.ep.get_stream_table()) + "\n")
