d19d494a8902c74d36942069ba3e7eedad33f5d5
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-12-06 23:34:32 +0000

    Updates

--- onode/server/probe_thread.py
+++ onode/server/probe_thread.py
@@ -154,11 +154,14 @@ class ProbeThread(threading.Thread):
 
     def handle_neighbour_measure(self, neighbour):
         loss_measured, delay_measured, last_packet = self.measure(neighbour)
-        if last_packet is not None and last_packet.type == PacketType.RMEASURE:
+        if not self.state.rendezvous and last_packet is not None and last_packet.type == PacketType.RMEASURE:
             self.handle_neighbour_response(neighbour, last_packet, delay_measured, loss_measured)
         else:
-            self.handle_neighbour_death(neighbour)
-            self.increment_sleep()
+            if self.state.rendezvous:
+                self.state.remove_neighbour_from_stream(neighbour)
+            else:
+                self.handle_neighbour_death(neighbour)
+                self.increment_sleep()
 
     def run(self):
         self.running = True
@@ -169,10 +172,10 @@ class ProbeThread(threading.Thread):
             if self.state.debug:
                 print(f"DEBUG: Sending the probe message to: {neighbours}")
 
-            if not self.state.rendezvous:
-                for neighbour in neighbours:
-                    threading.Thread(target=self.handle_neighbour_measure, args=(neighbour,)).start()
-            else:
+            for neighbour in neighbours:
+                threading.Thread(target=self.handle_neighbour_measure, args=(neighbour,)).start()
+
+            if self.state.rendezvous:
                 servers = self.state.get_servers()
                 print("SERVERS: " + str(servers))
                 if self.state.debug:
